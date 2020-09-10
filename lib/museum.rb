6632ba1527ed1766d6317dfe4bc418a0731a526c
Sean Steel <seanrsteel@gmail.com>
2020-09-10 13:56:00 -0600

    Add ticket_lottery_contestants method and test

--- lib/museum.rb
+++ lib/museum.rb
@@ -1,6 +1,5 @@
 class Museum
-  attr_reader :name
-  attr_accessor :exhibits, :patrons
+  attr_reader :name, :exhibits, :patrons
   def initialize(name)
     @name = name
     @exhibits = []
@@ -11,14 +10,22 @@ class Museum
     @exhibits << exhibit
   end
 
+  # def recommend_exhibits(patron)
+  #   patron.interests.map do |interest|
+  #     interest_exhibits = []
+  #     @exhibits.find_all do |exhibit|
+  #       exhibit.name == interest
+  #     end
+  #   end.flatten
+  # end
+#my way ^
+
   def recommend_exhibits(patron)
-    patron.interests.map do |interest|
-      interest_exhibits = []
-      @exhibits.find_all do |exhibit|
-        exhibit.name == interest
-      end
-    end.flatten
+    @exhibits.find_all do |exhibit|
+      patron.interests.include?(exhibit.name)
+    end
   end
+#kats way ^
 
   def admit(patron)
     @patrons << patron
@@ -31,11 +38,41 @@ class Museum
       @patrons.each do |patron|
         if patron.interests.include?(exhibit.name)
           patrons_array << patron
-
         end
       end
       exhibit_hash[exhibit] = patrons_array
     end
     exhibit_hash
   end
+#my way ^
+
+#   def patrons_by_exhibit_interest
+#     result = {}
+#     @exhibit.each do |exhibit|
+#       @patrons.each do |patron|
+#         results[exhibit] ||= []
+#         results[exhibit] << patron if patron.interests.include?(exhibit.name)
+#       end
+#     end
+#     results
+#   end
+#john's way ^
+#
+#   def patrons_by_exhibit_interest
+#     @exhibits.reduce(Hash.new(Array.new)) do |results, exhibit|
+#       results[exhibit] = @patrons.find_all do |patron|
+#         patron.interests.include?(exhibit.name)
+#       end
+#       results
+#     end
+#   end
+#hope's way ^
+
+  def ticket_lottery_contestants(exhibit)
+    patrons = patrons_by_exhibit_interest[exhibit]
+    patrons.find_all do |patron|
+      patron.spending_money < exhibit.cost
+    end 
+  end
+
 end
